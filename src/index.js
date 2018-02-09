2e97db7a8851520252a666465f851c1447be1906
gwens <gwenanspearing@hotmail.com>
2018-02-09 14:15:26 +0100

    Image is now displayed in the React component instead of vanilla JS

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-import './style.css';
-import Goose from './goose.jpg';
-
-console.log('hello webpack');
-
-// Use some imported CSS
-let nonReactStuff = document.getElementById('somestuff');
-
-// Use an imported image
-let myGoose = new Image();
-myGoose.src = Goose;
-nonReactStuff.appendChild(myGoose);
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
+import Goose from './goose.jpg';
 //require("babel-core").transform("code", options);
 
 ReactDOM.render(
   <div className="hello">
     <h1>Hello, React!</h1>
+    <img className="goose" src={Goose} width="400" />
   </div>,
   document.getElementById('root')
 );

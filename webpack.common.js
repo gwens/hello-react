e4d3de3a61f0dcc08b4dbcd1f1f4aac8085b98db
gwens <gwenanspearing@hotmail.com>
2018-02-13 15:14:55 +0100

    Put bundle.js for dev build in temp folder but now source location is wrong for prod build

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,5 @@
-const path = require('path');
-
 module.exports = {
   entry: './src/index.js',
-  devServer: {
-    contentBase: "./dist"
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,10 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  output: {
+    publicPath: "/temp/",
+    filename: "bundle.js"
+  },
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
